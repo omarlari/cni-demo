86f0f48f9c0faba36e6f238ee28863f10aa898cf
EC2 Default User <ec2-user@ip-172-31-27-146.us-west-2.compute.internal>
2019-04-21 23:51:51 +0000

    demo changes

--- demo.sh
+++ demo.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
-
+apt-get install pv
 ########################
 # include the magic
 ########################
 . demo-magic.sh
 
 # hide the evidence
-#clear
+clear
 
-# Put your stuff here
-pe "ls"
+# setup file system
+pe "mkdir ~/cni"
+pe "cd ~/cni"
+
+# Obtain binaries
+pe "curl -O -L https://github.com/containernetworking/cni/releases/download/v0.6.0/cni-amd64-v0.6.0.tgz"
+pe "https://github.com/containernetworking/plugins/releases/download/v0.7.5/cni-plugins-amd64-v0.7.5.tgz"
+
+# unpack cni-tools
+pe "tar -xzvf cni-amd64-v0.6.0.tgz"
+pe "tar -xzvf cni-plugins-amd64-v0.7.5.tgz"
+
+# clean up tar files
+pe "rm -rf *.tgz"
+
+
+
+
+
